3f095df2ffc57a743b79de77b21763f80b995846
Metchul <126379256+Metchul@users.noreply.github.com>
2023-02-28 20:10:00 -0800

    Craeted the todoitem class and it's constructor

--- Notes App/MainWindow.xaml.cs
+++ Notes App/MainWindow.xaml.cs
@@ -24,5 +24,7 @@ namespace Notes_App
         {
             InitializeComponent();
         }
+
+        
     }
 }
---
+++ Notes App/todoObject.cs
@@ -0,0 +1,30 @@
+﻿using Notes_App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace Notes_App
+{
+    public class TodoItem
+    {
+        public string TaskName { get; set; }
+        public Guid TodoId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool CompletedState { get; set; }
+        public DateTime CompletedDate { get; set; }
+
+        public TodoItem(string taskName)
+        {
+            TaskName = taskName;
+            CreatedDate = DateTime.Now;
+            CompletedState = false;
+            TodoId = Guid.NewGuid();
+        }
+    }
+
+    
+}
+
